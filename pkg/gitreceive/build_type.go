58acf21c86b03776e0ec8db8f6cf1ac946caf706
Nat Eguchi <nat@universlabs.co.uk>
2021-09-06 14:31:04 +0100

    Default is slug build

--- pkg/gitreceive/build_type.go
+++ pkg/gitreceive/build_type.go
@@ -3,8 +3,6 @@ package gitreceive
 import (
 	"fmt"
 	"os"
-
-	"github.com/teamhephy/controller-sdk-go/api"
 )
 
 type buildType string
@@ -18,28 +16,10 @@ const (
 	buildTypeDockerfile buildType = "dockerfile"
 )
 
-func getBuildType(dirName string, config api.Config) buildType {
-
-	hasDockerfile := false
-	if _, err := os.Stat(fmt.Sprintf("%s/Dockerfile", dirName)); err == nil {
-		hasDockerfile = true
+func getBuildTypeForDir(dirName string) buildType {
+	_, err := os.Stat(fmt.Sprintf("%s/Dockerfile", dirName))
+	if err == nil {
+		return buildTypeDockerfile
 	}
-
-	hasProcfile := false
-	if _, err := os.Stat(fmt.Sprintf("%s/Procfile", dirName)); err == nil {
-		hasProcfile = true
-	}
-	if hasDockerfile && hasProcfile {
-		if bTypeInterface, ok := config.Values["HEPHY_BUILDER"]; ok {
-			if strType, ok := bTypeInterface.(string); ok {
-				bType := buildType(strType)
-				if bType == buildTypeProcfile || bType == buildTypeDockerfile {
-					return bType
-				}
-			}
-		}
-	} else if hasProcfile {
-		return buildTypeProcfile
-	}
-	return buildTypeDockerfile
+	return buildTypeProcfile
 }
